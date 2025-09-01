e8941590d198ca1d77bb6ff2c418978e1121431f
jermainedeymos-gif <jermainedeymos@gmail.com>
2025-09-02 07:59:38 +0800

    Add files via upload
    first to commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+//include("");
+//include("");
+//include("");
+//include("");
+?>
+<form action="" method="">
+    <label for="">User_name:</label>
+    <br>
+    <input type="text" name="user" placeholder="Username..." required>
+    <br><br>
+
+    <label for="">User_password:</label>
+    <br>
+    <input type="password" name="pass" placeholder="Userpass..." required>
+    <br><br>
+
+    <input type="submit">
+</form>
