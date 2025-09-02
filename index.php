d48014760aaae3abbcc3947d21334574fff94a1d
AkoLangTo <phase2e2ps@gmail.com>
2025-09-02 09:06:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,22 @@
 //include("");
 //include("");
 ?>
-<form action="" method="">
-    <label for="">User_name:</label>
+<link rel="stylesheet" href="./assets/custom.css">
+<form action="./views/index.php" method="">
+    <h1>Client User Account</h1>
+    <p>Please login to view the dashboard</p>
+    <hr><br><br>
+
+    <label id="label-form" for="">User name:</label>
     <br>
-    <input type="text" name="user" placeholder="Username..." required>
+    <input id="input-form" type="text" name="user" placeholder="Username..." required>
     <br><br>
 
-    <label for="">User_password:</label>
+    <label id="label-form" for="">User password:</label>
     <br>
-    <input type="password" name="pass" placeholder="Userpass..." required>
-    <br><br>
+    <input id="input-form" type="password" name="pass" placeholder="Userpass..." required>
+    <br><br><hr><br><br>
 
     <input type="submit">
+    <br><br>
 </form>
