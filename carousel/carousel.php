85316740448af8fb01e2907d60866b8a8fbdebfe
jermainedeymos-gif <jermainedeymos@gmail.com>
2025-09-03 12:01:54 +0800

    Add files via upload
    Removing the old version of this repository and add the new and improve version

---
+++ carousel/carousel.php
@@ -0,0 +1,11 @@
+<style>
+    .video{
+        margin-left: 5%;
+        height: 600px;
+        object-fit: cover;
+    }
+</style>
+
+<video class="video"
+    src="https://framerusercontent.com/assets/jPGmgjk0UnrlNS7OFk2YvyM4c.mp4" loop="" muted="" playsinline="" autoplay="" width="90%">
+</video>
---
+++ index.php
@@ -0,0 +1,26 @@
+
+<?php
+//include("");
+//include("");
+//include("");
+//include("");
+?>
+<link rel="stylesheet" href="./assets/custom.css">
+<form action="./views/index.php" method="">
+    <h1>Client User Account</h1>
+    <p>Please login to view the dashboard</p>
+    <hr><br><br>
+
+    <label id="label-form" for="">User name:</label>
+    <br>
+    <input id="input-form" type="text" name="user" placeholder="Username..." required>
+    <br><br>
+
+    <label id="label-form" for="">User password:</label>
+    <br>
+    <input id="input-form" type="password" name="pass" placeholder="Userpass..." required>
+    <br><br><hr><br><br>
+
+    <input type="submit">
+    <br><br>
+</form>
