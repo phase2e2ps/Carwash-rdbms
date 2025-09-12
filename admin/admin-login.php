06ca10bafc49d3ba711c9a1c32b146ad49c2f724
jermainedeymos-gif <jermainedeymos@gmail.com>
2025-09-12 11:20:09 +0800

    Update admin-login.php

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+if (isset($_SESSION['admin_id'])) {
+    header("Location: ./index.php"); // already logged in → go to admin dashboard
+    exit;
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,65 +18,71 @@
 </head>
 <style>
     /*===Client Login Form===*/
-#body{
-    background-image: url('https://wallpapers.com/images/hd/silver-aesthetic-car-wash-av2eptgo6na7ybh8.jpg');
-    background-repeat: no-repeat;
-    background-position: cover;
-}
-#index{
-    border: 3px solid #f1f1f1;
-    width: 19%;
-    margin: auto;
-    margin-top: 13%;
-    padding: 10px;
-    background-color: white;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    background: linear-gradient(135deg, #74ebd5 0%, #ACB6E5 100%);
-    border-radius: 7%;
-}
-#input-form{
-    width: 90%;
-    padding: 12px 20px;
-    margin: 8px 18px;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
+    #body {
+        background-image: url('https://wallpapers.com/images/hd/silver-aesthetic-car-wash-av2eptgo6na7ybh8.jpg');
+        background-repeat: no-repeat;
+        background-position: cover;
+    }
 
-}
-#label-form{
-    font-weight: bold;
-    margin-left: 5%;
-}
+    #index {
+        border: 3px solid #f1f1f1;
+        width: 19%;
+        margin: auto;
+        margin-top: 13%;
+        padding: 10px;
+        background-color: white;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        background: linear-gradient(135deg, #74ebd5 0%, #ACB6E5 100%);
+        border-radius: 7%;
+    }
 
+    #input-form {
+        width: 90%;
+        padding: 12px 20px;
+        margin: 8px 18px;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
 
+    }
 
+    #label-form {
+        font-weight: bold;
+        margin-left: 5%;
+    }
 
+    #a {
+        text-align: center;
+    }
 
-
-
-
-
+    #b {
+        margin-left: 22%;
+    }
 </style>
-<body id="body">
-    <form id="index" action="./admin-dashboard.php" method="post">
 
-        <h1>Client User Account</h1>
+<body id="body">
+    <form id="index" action="../page/admin-log-handler.php" method="post">
+
+        <h1>Admin User Account</h1>
         <p>Please login to view the dashboard</p>
         <hr><br><br><br>
 
         <label id="label-form" for="">User Name:</label>
         <br>
-        <input id="input-form" type="text" name="user" placeholder="Username..." required>
+        <input id="input-form" type="text" name="username" placeholder="Username..." required>
         <br>
 
         <label id="label-form" for="">User Password:</label>
         <br>
-        <input id="input-form" type="password" name="pass" placeholder="Userpass..." required>
+        <input id="input-form" type="password" name="password" placeholder="Userpass..." required>
         <br><br>
         <hr><br><br>
 
-        <input name="submit" type="submit">
+        <input type="submit" name="login" value="Login">
         <br><br>
+
+        <p id="a">------------------Or------------------</p>
+        <label id="b" for="">Don't have an account? <a id="c" href="./admin-register.php">Register</a></label>
     </form>
 </body>
 
